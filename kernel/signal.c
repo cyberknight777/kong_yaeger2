b8b5c3da6cd6a8a71e85b78bce6e6d3c7f4cd814
TheMalachite <eliasgheeraert@gmail.com>
2020-08-02 22:53:27 +0200

    [BACKPORT] treewide: Backport CAF msm-4.14 low memory killer changes
    Signed-off-by: cloudprject <sultantammam3@gmail.com>

--- kernel/signal.c
+++ kernel/signal.c
@@ -44,7 +44,6 @@
 #include <linux/posix-timers.h>
 #include <linux/oom.h>
 #include <linux/capability.h>
-#include <linux/cgroup.h>
 
 #define CREATE_TRACE_POINTS
 #include <trace/events/signal.h>
@@ -1349,8 +1348,11 @@ int group_send_sig_info(int sig, struct siginfo *info, struct task_struct *p)
 	ret = check_kill_permission(sig, info, p);
 	rcu_read_unlock();
 
-	if (!ret && sig)
+	if (!ret && sig) {
 		ret = do_send_sig_info(sig, info, p, true);
+		if (capable(CAP_KILL) && sig == SIGKILL)
+			add_to_oom_reaper(p);
+	}
 
 	return ret;
 }

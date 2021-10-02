f907c591aa7ac2b13bac0075e17bef64bd282220
nil <shas45558@gmail.com>
2021-10-02 01:11:03 -0400

    BIg cpu freq now 500 mhz

--- drivers/misc/mediatek/base/power/cpufreq_v1/src/mach/mt6768/mtk_cpufreq_opp_table.h
+++ drivers/misc/mediatek/base/power/cpufreq_v1/src/mach/mt6768/mtk_cpufreq_opp_table.h
@@ -688,20 +688,20 @@
 #define CPU_DVFS_FREQ15_LL_G75		500000		/* KHz */
 
 /* for DVFS OPP table B */
-#define CPU_DVFS_FREQ0_L_G75		2000000		/* KHz */
-#define CPU_DVFS_FREQ1_L_G75		1950000		/* KHz */
-#define CPU_DVFS_FREQ2_L_G75		1900000		/* KHz */
-#define CPU_DVFS_FREQ3_L_G75		1850000		/* KHz */
-#define CPU_DVFS_FREQ4_L_G75		1800000		/* KHz */
-#define CPU_DVFS_FREQ5_L_G75		1710000		/* KHz */
-#define CPU_DVFS_FREQ6_L_G75		1621000		/* KHz */
-#define CPU_DVFS_FREQ7_L_G75		1532000		/* KHz */
-#define CPU_DVFS_FREQ8_L_G75		1443000		/* KHz */
-#define CPU_DVFS_FREQ9_L_G75		1354000		/* KHz */
-#define CPU_DVFS_FREQ10_L_G75		1295000		/* KHz */
-#define CPU_DVFS_FREQ11_L_G75		1176000		/* KHz */
-#define CPU_DVFS_FREQ12_L_G75		1087000		/* KHz */
-#define CPU_DVFS_FREQ13_L_G75		998000		/* KHz */
+#define CPU_DVFS_FREQ0_L_G75		1790000		/* KHz */
+#define CPU_DVFS_FREQ1_L_G75		1680000		/* KHz */
+#define CPU_DVFS_FREQ2_L_G75		1640000		/* KHz */
+#define CPU_DVFS_FREQ3_L_G75		1590000		/* KHz */
+#define CPU_DVFS_FREQ4_L_G75		1532000		/* KHz */
+#define CPU_DVFS_FREQ5_L_G75		1473000		/* KHz */
+#define CPU_DVFS_FREQ6_L_G75		1414000		/* KHz */
+#define CPU_DVFS_FREQ7_L_G75		1354000		/* KHz */
+#define CPU_DVFS_FREQ8_L_G75		1295000		/* KHz */
+#define CPU_DVFS_FREQ9_L_G75		1176000		/* KHz */
+#define CPU_DVFS_FREQ10_L_G75		1117000		/* KHz */
+#define CPU_DVFS_FREQ11_L_G75		1057000		/* KHz */
+#define CPU_DVFS_FREQ12_L_G75		998000		/* KHz */
+#define CPU_DVFS_FREQ13_L_G75		939000		/* KHz */
 #define CPU_DVFS_FREQ14_L_G75		774000		/* KHz */
 #define CPU_DVFS_FREQ15_L_G75		500000		/* KHz */
 
@@ -758,7 +758,6 @@
 #define CPU_DVFS_VOLT13_VPROC2_G75	 70125		/* 10uV */
 #define CPU_DVFS_VOLT14_VPROC2_G75	 64500		/* 10uV */
 #define CPU_DVFS_VOLT15_VPROC2_G75	 62000		/* 10uV */
->>>>>>> 08e5fb545803... Cpu under voli -50mv (First 5 steps)
 
 /* for DVFS OPP table CCI */
 #define CPU_DVFS_VOLT0_VPROC3_G75	100125		/* 10uV */
